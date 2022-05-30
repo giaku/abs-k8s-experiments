5dcd58066f7253e8dd3d939f8c2cffe4ce3c2981
Gianluca Turin <gianluca.turin@gmail.com>
2022-05-30 20:07:08 +0200

    Update slices.sh

--- stress-test-triplets/helpers/slices.sh
+++ stress-test-triplets/helpers/slices.sh
@@ -30,7 +30,6 @@ BEGIN {
       split(tokenz[2], times, ":")
 
       START=mktime(tokens[1]" "tokens[2]" "tokens[3]" "times[1]" "times[2]" "times[3])
-      #print "START "tokens[1]" "tokens[2]" "tokens[3]" "times[1]" "times[2]" "times[3]
       debug1=""tokens[1]" "tokens[2]" "tokens[3]" "times[1]" "times[2]" "times[3]
       split(endtime[1], tokenz, "_")
       split(tokenz[1], tokens, "-")
@@ -41,7 +40,6 @@ BEGIN {
       split(tokenz[2], times, ":")
 
       FINE=mktime(tokens[1]" "tokens[2]" "tokens[3]" "times[1]" "times[2]" "times[3])
-      #print "FINE "tokens[1]" "tokens[2]" "tokens[3]" "times[1]" "times[2]" "times[3]
       debug2=""tokens[1]" "tokens[2]" "tokens[3]" "times[1]" "times[2]" "times[3]
       split($0, tokens, ",")
       split(tokens[1], datetokens, " ")
@@ -49,31 +47,15 @@ BEGIN {
       split(datetokens[2], timetokens, ":")
     
       CURRENT=mktime(datetokenz[1]" "datetokenz[2]" "datetokenz[3]" "timetokens[1]" "timetokens[2]" "timetokens[3])
-      #print "CURRENT "datetokenz[1]" "datetokenz[2]" "datetokenz[3]" "timetokens[1]" "timetokens[2]" "timetokens[3]
       debug3=""datetokenz[1]" "datetokenz[2]" "datetokenz[3]" "timetokens[1]" "timetokens[2]" "timetokens[3]
-      #print START" @@@@@ "CURRENT" @@@@@ "FINE
-      #CURRENT=CURRENT-7200
       F=folder""filename"-"starttime[i]"-iteration-"i".csv"
 #print F
-#      if (F=="./150-wf2-100-wf3/timeintervals-2021-05-18_00:12:27-iteration-2.csv") {print "Discarded\n epoch: "START" @@@@@ "CURRENT" @@@@@ "FINE"\nreadable: "debug1" @@@@@ "debug3" @@@@ "debug2}
       if(NR==3) {print HEADERS > F}
       if(CURRENT>=START && CURRENT<=FINE) {print $0 >> F} 
-      #else {print "discarded"}
-      #end=start+OFF2
-      #start=start+OFF1
-   
-      #startd=strftime("%y-%m-%d-%H:%M:%S",start)
-      #endd=strftime("%y-%m-%d-%H:%M:%S",end)
-  
-      #print "Timestamp: " START " " FINE
-      #print " --- INTERVAL START: " startd " --- INTERVAL FINE: " endd
-      #print startd" "endd
   }}
 }}
 
 END {
-
   #print OFF1, OFF2
-
 }'
 
